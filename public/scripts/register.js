279a7637f4fa2a0312c5b79f7c056882fc1f4af9
kreeos <kanghyun.kris.lee@gmail.com>
2020-02-13 14:57:25 +0000

    Project Revised

--- public/scripts/register.js
+++ public/scripts/register.js
@@ -5,13 +5,12 @@ function submitListener() {
   else{
     window.alert("test");
   }
-
 }
 
-document.addEventListener("DOMContentLoaded", function(event) {
-  let button = document.getElementById("form_submit");
-  let test = document.getElementById("userid");
-  console.log(button);
-  console.log(test.text);
-  button.addEventListener("click", submitListener);
-});
+// document.addEventListener("DOMContentLoaded", function(event) {
+//   let button = document.getElementById("form_submit");
+//   let test = document.getElementById("userid");
+//   console.log(button);
+//   console.log(test.text);
+//   button.addEventListener("click", submitListener);
+// });
